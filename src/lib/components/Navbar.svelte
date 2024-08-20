5ad888e913d8866df8b443761dda6f92d30e9514
serbog <serbogatinoski@gmail.com>
2024-08-20 20:56:34 +0200

    Changed Nav

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,12 @@
 <script>
-	import CreatePost from '$lib/components/CreatePost.svelte';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
 	import { page } from '$app/stores';
+	import CreatePost from '$lib/components/CreatePost.svelte';
 	import Logo from '$lib/components/Logo.svelte';
+	import { Button, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
 	$: activeUrl = $page.url.pathname;
 </script>
 
-<Navbar class="font-roboto bg-transparent">
+<Navbar class="bg-transparent font-roboto">
 	<NavBrand href="/">
 		<div class="w-32">
 			<Logo />
@@ -18,8 +18,8 @@
 	</div>
 	<NavUl {activeUrl}>
 		<NavLi activeClass="text-primary-700" class="text-xl" href="/">Home</NavLi>
-		<NavLi activeClass="text-primary-700" class="text-xl" href="/young">Young</NavLi>
-		<NavLi activeClass="text-primary-700" class="text-xl" href="/old">Old</NavLi>
+		<NavLi activeClass="text-primary-700" class="text-xl" href="/young">Youth Posts</NavLi>
+		<NavLi activeClass="text-primary-700" class="text-xl" href="/old">Elderly Posts</NavLi>
 		<NavLi activeClass="text-primary-700" class="text-xl" href="/profile">Profile</NavLi>
 	</NavUl>
 </Navbar>
